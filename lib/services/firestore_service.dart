7f1f4f7b86a1a2ae9c8d48e8289d078e1c0b8e40
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-07-19 23:10:25 -0500

    Service 2.2

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,5 +1,6 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:menuapp/models/product_model.dart';
 
 class FirestoreService{
 
@@ -9,13 +10,15 @@ class FirestoreService{
 
   late final CollectionReference _collectionReference = FirebaseFirestore.instance.collection(collection);
 
-  Future<List> getProducts() async{
-    List<Map<String, dynamic>> products = [];
+  Future<List<ProductModel>> getProducts() async{
+    List<ProductModel> products = [];
     QuerySnapshot _querySnapshot =  await _collectionReference.get();
     _querySnapshot.docs.forEach((element) {
-      Map<String, dynamic> product = element.data() as Map<String, dynamic>;
-      product["id"] = element.id;
-      products.add(product);
+      // Map<String, dynamic> product = element.data() as Map<String, dynamic>;
+      // product["id"] = element.id;
+      ProductModel productModel = ProductModel.fromJson(element.data() as Map<String, dynamic>);
+      productModel.id = element.id;
+      products.add(productModel);
     });
     return products;
   }
