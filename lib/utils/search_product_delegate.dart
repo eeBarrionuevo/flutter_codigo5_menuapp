c9f165053f25ca6c73403d94ee0ef393461f0e3c
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-07-21 19:49:26 -0500

    SeachDelegate 3

--- lib/utils/search_product_delegate.dart
+++ lib/utils/search_product_delegate.dart
@@ -4,26 +4,27 @@ import 'package:flutter/material.dart';
 class SearchProductDelegate extends SearchDelegate {
   @override
   List<Widget>? buildActions(BuildContext context) {
-    // TODO: implement buildActions
-    throw UnimplementedError();
+    return [
+      Text("buildActions", style: TextStyle(color: Colors.redAccent,),),
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
     // TODO: implement buildLeading
-    throw UnimplementedError();
+    return Text("buildLeading", style: TextStyle(color: Colors.redAccent,),);
   }
 
   @override
   Widget buildResults(BuildContext context) {
     // TODO: implement buildResults
-    throw UnimplementedError();
+    return Text("buildResults");
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // TODO: implement buildSuggestions
-    throw UnimplementedError();
+    return Text("buildSuggestions");
   }
   
 }
