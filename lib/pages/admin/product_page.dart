56f1f4a0db89ad185336fde2cc41c3994a221a29
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-07-22 21:43:17 -0500

    ProductPage 1.5

--- lib/pages/admin/product_page.dart
+++ lib/pages/admin/product_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:menuapp/ui/general/colors.dart';
@@ -7,6 +8,7 @@ import 'package:menuapp/ui/widgets/text_widget.dart';
 
 class ProductPage extends StatelessWidget {
 
+  final CollectionReference _productReference = FirebaseFirestore.instance.collection('products');
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +17,27 @@ class ProductPage extends StatelessWidget {
         backgroundColor: kBrandPrimaryColor,
         title: Text("Productos"),
       ),
-      body: ListView.builder(
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          return ItemAdminProductWidget();
+      body: StreamBuilder(
+        stream: _productReference.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snap){
+          if(snap.hasData){
+            return ListView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return ItemAdminProductWidget();
+              },
+            );
+          }
+          return Center(
+            child: SizedBox(
+              width: 20,
+              height: 20,
+              child: CircularProgressIndicator(
+                strokeWidth: 2,
+                color: kBrandSecondaryColor,
+              ),
+            ),
+          );
         },
       ),
     );
