2794f2cf2161cbf837ce775ff0bf02ae34b7a89a
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-07-22 20:26:30 -0500

    HomeAdmin 1.6

--- lib/pages/admin/product_page.dart
+++ lib/pages/admin/product_page.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:menuapp/ui/general/colors.dart';
 
 class ProductPage extends StatelessWidget {
   const ProductPage({Key? key}) : super(key: key);
@@ -8,7 +8,42 @@ class ProductPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: kBrandPrimaryColor,
         title: Text("Productos"),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(14.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.05),
+                  blurRadius: 10,
+                  offset: const Offset(4, 4),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30.0,
+                  backgroundImage: NetworkImage(
+                      "https://comidaperuanaweb.org/wp-content/uploads/2018/10/Rocoto-Relleno-1-1-1.png"),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
