534ba72a9dd96fe5a28659602f862627aa8e8d48
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-07-22 21:20:26 -0500

    ProductPage 1.3

--- lib/pages/admin/product_page.dart
+++ lib/pages/admin/product_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:menuapp/ui/general/colors.dart';
 import 'package:menuapp/ui/widgets/general_widget.dart';
 import 'package:menuapp/ui/widgets/text_widget.dart';
@@ -46,16 +47,15 @@ class ProductPage extends StatelessWidget {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Container(
-                            padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 8.0, vertical: 2.0),
                             decoration: BoxDecoration(
                               color: kBrandSecondaryColor.withOpacity(0.75),
                               borderRadius: BorderRadius.circular(20.0),
                             ),
                             child: Text(
                               "Plato de fondo",
-                              style: TextStyle(
-                                  fontSize: 12.0
-                              ),
+                              style: TextStyle(fontSize: 12.0),
                             ),
                           ),
                           divider3,
@@ -71,7 +71,7 @@ class ProductPage extends StatelessWidget {
                             textOverflow: TextOverflow.ellipsis,
                             color: kBrandPrimaryColor.withOpacity(.6),
                             text:
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
                           ),
                           divider6,
                           Row(
@@ -86,11 +86,33 @@ class ProductPage extends StatelessWidget {
                   right: -12,
                   top: -10,
                   child: PopupMenuButton(
-                    icon: Icon(Icons.more_vert, color: kBrandPrimaryColor.withOpacity(0.8),),
-                    itemBuilder: (context){
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    icon: Icon(
+                      Icons.more_vert,
+                      color: kBrandPrimaryColor.withOpacity(0.8),
+                    ),
+                    itemBuilder: (context) {
                       return [
-                        PopupMenuItem(child: Text("Actualizar",),),
-                        PopupMenuItem(child: Text("Eliminar",),),
+                        PopupMenuItem(
+                          child: Row(
+                            children: [
+                              SvgPicture.asset('assets/icons/edit.svg'),
+                              dividerWidth6,
+                              TextNormal(text: "Actualizar",),
+                            ],
+                          ),
+                        ),
+                        PopupMenuItem(
+                          child: Row(
+                            children: [
+                              SvgPicture.asset('assets/icons/trash.svg'),
+                              dividerWidth6,
+                              TextNormal(text: "Eliminar",),
+                            ],
+                          ),
+                        ),
                       ];
                     },
                   ),
