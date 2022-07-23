36e34c96b96c559c3abecf72b33274b473365879
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-07-22 22:22:44 -0500

    ProductPage 2.5

--- lib/pages/admin/product_form_page.dart
+++ lib/pages/admin/product_form_page.dart
@@ -18,7 +18,46 @@ class ProductFormPage extends StatelessWidget {
           padding: const EdgeInsets.all(16),
           child: Column(
             children: [
-
+              Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.05),
+                      blurRadius: 12.0,
+                      offset: const Offset(4, 4),
+                    )
+                  ],
+                ),
+                child: TextFormField(
+                  style: const TextStyle(
+                    fontSize: 14.0,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: "Nombre producto",
+                    hintStyle: const TextStyle(
+                      fontSize: 14.0,
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
