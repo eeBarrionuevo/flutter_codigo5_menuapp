65c0d3e91376eb095ce81046c15a025078a01854
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-07-30 12:25:47 -0500

    SharedPreferences 3

--- lib/helpers/sp_global.dart
+++ lib/helpers/sp_global.dart
@@ -15,4 +15,25 @@ class SPGlobal {
     _prefs = await SharedPreferences.getInstance();
   }
 
+  set fullName(String value){
+    _prefs.setString("fullName", value);
+  }
+
+  String get fullName => _prefs.getString('fullName') ?? "";
+
+
+  set email(String value){
+    _prefs.setString("email", value);
+  }
+
+  String get email => _prefs.getString('email') ?? "";
+
+
+  set isLogin(bool value){
+    _prefs.setBool("isLogin", value);
+  }
+
+  bool get isLogin => _prefs.getBool('isLogin') ?? false;
+
+
 }
