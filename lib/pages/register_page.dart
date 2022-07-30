9f17be4c3de3728cf7664269a0a89269f8476932
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-07-30 09:53:46 -0500

    Register Page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,13 +1,36 @@
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatelessWidget {
-  const RegisterPage({Key? key}) : super(key: key);
+
+  registerCustomer() async {
+    UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: "mandarina@gmail.com",
+      password: "3volution",
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
+    return  Container(
+      constraints: const BoxConstraints.expand(),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage("https://images.pexels.com/photos/1108117/pexels-photo-1108117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Column(
+            children: [
+              TextField(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
